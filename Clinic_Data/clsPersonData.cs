41ba7c6c89272e0fe36414d67a3e614340b6af0b
Abazr Amin Hamza <azr032024@gmail.com>
2025-01-28 23:58:06 +0300

    Start Project

---
+++ Clinic_Data/clsPersonData.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic_Data
+{
+    public class clsPersonData
+    {
+        public static bool GetPersonByID(int PersonID, ref string Name, ref DateTime DateOfBirth, ref byte Gendor,
+            ref string Phone, ref string Email, ref string Address)
+        {
+            bool isFound = false;
+
+            SqlConnection connection = new SqlConnection(clsSetting.connection);
+
+            string query = "select * from Person where PersonID = @PersonID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@PersonID", PersonID);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    isFound = true;
+
+                    Name = (string)reader["Name"];
+                    DateOfBirth = (DateTime)reader["DateOfBirth"];
+                    Gendor = (byte)reader["Gendor"];
+                    Phone = (string)reader["Phone"];
+                    Email = (string)reader["Email"];
+                    Address = (string)reader["Address"];
+
+                }
+                else
+                {
+                    isFound = false;
+                }
+
+                reader.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return isFound;
+        }
+    }
+}
